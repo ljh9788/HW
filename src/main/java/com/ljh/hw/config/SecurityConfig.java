1574acb19a26a30636fac26e443a33a5f67f165c
ljh <junho080897@gmail.com>
2025-05-06 23:58:50 +0900

    250506 HW - ver 1.0.2
    의존성 추가
    - thymeleaf , SecurityCOnfig
    
    Config 추가
    - SecurityConfig.java

---
+++ src/main/java/com/ljh/hw/config/SecurityConfig.java
@@ -0,0 +1,28 @@
+package com.ljh.hw.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((authz) -> authz
+                        .requestMatchers("/public/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin(withDefaults());
+
+        return http.build();
+    }
+}
--- src/main/java/com/ljh/hw/controller/MainController.java
+++ src/main/java/com/ljh/hw/controller/MainController.java
@@ -1,18 +1,21 @@
 package com.ljh.hw.controller;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/")
 public class MainController {
 
-    @GetMapping("/index")
-    public String index() {
-        System.out.println("index");
-        return "index.html";
+    private static final Logger log = LoggerFactory.getLogger(MainController.class);
 
+    @GetMapping("/")
+    public String login() {
+        log.info("Login 진입");
+        return "/user/login";
     }
+
 }
