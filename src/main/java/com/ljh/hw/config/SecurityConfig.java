4a16fcd7958247f55746166d6e580596a78d61ce
ljh <junho080897@gmail.com>
2025-05-11 23:27:09 +0900

    250511 HW - ver 1.0.2
    - Security 설정
    - 로그인 기본페이지 구성

--- src/main/java/com/ljh/hw/config/SecurityConfig.java
+++ src/main/java/com/ljh/hw/config/SecurityConfig.java
@@ -14,14 +14,24 @@ import static org.springframework.security.config.Customizer.withDefaults;
 public class SecurityConfig {
 
 
+    /*  Spring Security 보안필터
+     *  permitAll : 제한없이 모두접근가능 페이지
+     *  anyRequest().authenticated : 나머지 요청은 인증된 사용자만 접근가능
+     * => 추후 권한에따른 접근url 제한설정  ex) 관리자 , 운영진
+    */
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests((authz) -> authz
-                        .requestMatchers("/public/**").permitAll()
+                        .requestMatchers("/", "/login", "/css/**" , "/js/**").permitAll()
                         .anyRequest().authenticated()
                 )
-                .formLogin(withDefaults());
+                .formLogin((form) -> form
+                        .loginPage("/login")
+                        .defaultSuccessUrl("/home", true)
+                        .permitAll())
+                .logout((logout) -> logout
+                        .logoutSuccessUrl("/login?logout"));
 
         return http.build();
     }
